5f4ea6e2c28a7817e674bbfd0328e7cf27bb0a65
emmysilly <emelybchavez@gmail.com>
2018-08-08 19:39:19 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,3 +1,18 @@
-// Write your code in this file!
+function getUKTime() {
+ var result = new Date().getHours() + 5;
+ return result;
+}
+ 
+getUKTime();
 
 
+
+
+function doubleTheInput(input) {
+  return input * ;
+}
+ 
+doubleTheInput(5)
+doubleTheInput(1)
+doubleTheInput(-40)
+doubleTheInput(500)
